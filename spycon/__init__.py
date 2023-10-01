baf5eb3fd837989f7c397d3f1d4f2201589d44be
Christian Donner <chdonner@ethz.ch>
2023-10-01 11:34:26 +0200

    chore:clean up

--- spycon/__init__.py
+++ spycon/__init__.py
@@ -1,2 +1,5 @@
 from . import coninf
 from . import benchmark, spycon_inference, spycon_result, spycon_tests
+from .spycon_tests import ConnectivityTest, load_test
+from .spycon_result import SpikeConnectivityResult, load_connectivity_result
+from .benchmark import ConnectivityBenchmark
